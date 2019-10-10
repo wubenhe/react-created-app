49b9fcc3519d5ef418cd504925218b21802fceb7
Ben Wu <ben@armorblox.com>
2019-10-09 17:24:04 -0700

    adding redux

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+
+
+const init = (state = false, action) => {
+    switch (action.type) {
+      default:
+        return state
+    }
+  }
+  
+export default combineReducers({init});
